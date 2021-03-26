325dea3cf73cb50b62c4900376ef42ad77fc3672
XX5CRISXX5 <80927246+XX5CRISXX5@users.noreply.github.com>
2021-03-25 18:48:55 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,1 +1,16 @@
-
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDf25qT2sD5wTjzTh1hmGEKddeLC_Kacm4",
+    authDomain: "autentifica41ce.firebaseapp.com",
+    projectId: "autentifica41ce",
+    storageBucket: "autentifica41ce.appspot.com",
+    messagingSenderId: "481669253171",
+    appId: "1:481669253171:web:cf2f06a4d9d24b62e1fb46",
+    measurementId: "G-R4B701KCD8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
